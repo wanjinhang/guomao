d508bbe3fbc2dc8c276d2103a482fbad9095c3ad
wansui <wanjinhang@gmail.com>
2017-10-18 15:04:54 +0800

    修改IP地址

--- src/com/allinpayjl/guomao/uitl/Url.java
+++ src/com/allinpayjl/guomao/uitl/Url.java
@@ -1,10 +1,10 @@
 package com.allinpayjl.guomao.uitl;
 
 public class Url {
-	public final static String webIndex="http://222.168.73.115/guomao/index.php/";//webView ???
-	public final static String appConnect ="http://222.168.73.115/guomao/index.php/Home/Index/appConnect.html";//
-	public final static String upDataUrl ="http://222.168.73.115/guomao/guomao.apk";
-//	public final static String webIndex="http://221.8.23.226:8082/";//??????????
-//	public final static String appConnect ="http://221.8.23.226:8082/index.php/Home/Index/appConnect.html";
-//	public final static String upDataUrl ="http://221.8.23.226:8082/apk/guomao.apk";
+//	public final static String webIndex="http://222.168.73.115/guomao/index.php/";//webView ???
+//	public final static String appConnect ="http://222.168.73.115/guomao/index.php/Home/Index/appConnect.html";//
+//	public final static String upDataUrl ="http://222.168.73.115/guomao/guomao.apk";
+	public static final String appConnect = "http://119.53.151.50:8082/index.php/Home/Index/appConnect.html";
+	public static final String upDataUrl = "http://119.53.151.50:8082/apk/guomao.apk";
+	public static final String webIndex = "http://119.53.151.50:8082/";
 }
